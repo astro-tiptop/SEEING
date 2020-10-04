5308a7e00df14e0ebb9b271f20adf06fd1c1b509
Fabio Rossi <frossi@faramir.arcetri.astro.it>
2020-10-04 15:51:55 +0200

    Adding some documentation and a new function to manipulate symbols names

--- sympyHelpers.py
+++ sympyHelpers.py
@@ -5,6 +5,21 @@ from lambdifyDictionaries import *
 
 
 def getSymbolByName(expr, sname):
+    """Gets a symbol from a sympy expressions, given the symbols name
+
+    Parameters
+    ----------
+    expr : sympy expression
+        A sympy expression 
+    sname : string
+        A string containing the symbol name
+
+    Returns
+    -------
+    Symbol
+        The symbol having the required name. None is returned if the symbol is not found
+    """
+
     free_syms = {symbol.name: symbol for symbol in expr.free_symbols}
     if sname in free_syms.keys():
         return free_syms[sname]
@@ -12,8 +27,24 @@ def getSymbolByName(expr, sname):
         return None
     
 
-def subsParamsByName(expr1, subsDict):
-    expr = expr1
+def subsParamsByName(expr, subsDict):
+    """Generate a new sympy expression from a given one by substitution of a set of its symbols. 
+    Substitutions are specified as a dictionay of string:value pairs (value can be any sympy expression 
+    or numerical value).
+
+    Parameters
+    ----------
+    expr : sympy expression
+        A sympy expression 
+    subsDict : dict
+        A dictionary containing the "symbol name":value substitution pairs
+
+    Returns
+    -------
+    expr
+        The resulting sympy expression
+    """    
+
     for sname, sval in subsDict.items():
         ss = getSymbolByName(expr, sname)
         if ss:
@@ -21,18 +52,54 @@ def subsParamsByName(expr1, subsDict):
     return expr
 
 
-def lambdifyByName(expr, symbolsNames, amodules):
+def lambdifyByName(expr, symbolsNames, modules_):
+    """Wraps around sympy.lambdify to generate a lambda function from a given sympy expression. 
+    The set of symbols to be passed to sympy.lambdify is specified as a list of strings. 
+
+    Parameters
+    ----------
+    expr : sympy expression
+        A sympy expression 
+    symbolsNames : string
+        A list of strings containing the symbols name
+    modules_ : see sympy lambdify modules 
+
+    Returns
+    -------
+    lambda
+        The resulting lambda 
+    """    
+
     symbolsList = []
     for sName in symbolsNames:
         symbolsList.append(getSymbolByName(expr, sName))
-    return sp.lambdify( symbolsList, expr, modules=amodules )
+    return sp.lambdify( symbolsList, expr, modules=modules_ )
 
 
-def getRestrictedLambdaBasic(
+def getRestrictedLambda(
         expr,
         subsDict,
         independentVarNames,
         lmodules=cpulib):
+    """Wraps around sympy.lambdify to generate a lambda function from a given sympy expression. 
+    The set of symbols to be passed to sympy.lambdify is specified as a list of strings. 
+    If one or more symbols are not found in the expressions, dummy symbols are introduced.
+
+    Parameters
+    ----------
+    expr : sympy expression
+        A sympy expression 
+    symbolsNames : string
+        A list of strings containing the symbols names
+    modules_ : see sympy lambdify modules 
+
+    Returns
+    -------
+    lambda
+        The resulting lambda 
+    """    
+
+    
     free_syms = {symbol.name: symbol for symbol in expr.free_symbols}
     for sname, sval in subsDict.items():
         expr = expr.subs(free_syms[sname], sval)
@@ -49,6 +116,24 @@ def getRestrictedLambdaBasic(
 
 
 def evaluateLambda(p_f, independentVarNames, samples, xp=np):
+    """Evaluates a given a ufunc (numpy or cupy)
+
+    Parameters
+    ----------
+    p_f : ufunc
+        A numpy or cupy ufunc 
+    independentVarNames : string list 
+        A list of strings containing the names of the symbols to be used as evaluations variables
+    samples : a list of numpy or cupy arrays 
+        A samples vector for each variable, to be used generate the grid to evaluate the ufunc
+    xp : Array library, either np (numpy) or cp (cupy)
+
+    Returns
+    -------
+    array
+        The result of the function evaluation 
+    """
+
     nvars = len(independentVarNames)
     if nvars==0:
         return None
@@ -67,7 +152,27 @@ def evaluateFormula(
         independentVarNames,
         samples,
         modules=cpulib):
-    p_f = getRestrictedLambdaBasic(expr, subsDict, independentVarNames, modules)
+    """Evaluates a given a sympy expression
+
+    Parameters
+    ----------
+    expr : sympy expressions
+        The expressions to be evaluated
+    subsDict : dict
+        A dictionary containing the "symbol name":value substitution pairs
+    independentVarNames : string list 
+        A list of strings containing the names of the symbols to be used as evaluations variables
+    samples : a list of numpy or cupy arrays 
+        A samples vector for each variable, to be used generate the grid to evaluate the expressions
+    modules : either cpulib (numpy) or gpulib (cupy)
+
+    Returns
+    -------
+    array
+        The result of the expressions evaluation 
+    """
+
+    p_f = getRestrictedLambdac(expr, subsDict, independentVarNames, modules)
     return evaluateLambda(p_f, independentVarNames, samples)
 
 
@@ -103,3 +208,14 @@ def sympyToString(expr):
 #
 #ssrE = parse_expr(ssr)
 #display(ssrE)
+
+
+def subsSpecialParams(expr, s1, s2):
+    
+    free_syms = {symbol.name: symbol for symbol in expr.free_symbols}
+
+    for sname in free_syms:
+        ss = getSymbolByName(expr, sname)
+        expr = expr.subs(ss, sp.symbols(sname.replace(s1, s2)))
+
+    return expr
